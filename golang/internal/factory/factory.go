692879c1d7a941d8a66b017d44b1d6b2d702a042
Nasher3296 <ignaciofernandez3296@gmail.com>
2026-03-29 17:24:53 -0300

    estructura

--- golang/internal/factory/factory.go
+++ golang/internal/factory/factory.go
@@ -1,13 +1,15 @@
 package factory
 
 import (
+	r "github.com/7574-sistemas-distribuidos/tp-mom/golang/internal/factory/rabbitmq"
 	m "github.com/7574-sistemas-distribuidos/tp-mom/golang/internal/middleware"
 )
 
 func CreateQueueMiddleware(queueName string, connectionSettings m.ConnSettings) (m.Middleware, error) {
-	return nil, nil
+	return r.NewQueueMiddleware(queueName, connectionSettings)
 }
 
 func CreateExchangeMiddleware(exchange string, keys []string, connectionSettings m.ConnSettings) (m.Middleware, error) {
+	// return r.NewExchangeMiddleware(exchange, keys, connectionSettings)
 	return nil, nil
 }
---
+++ golang/internal/factory/rabbitmq/queue.go
@@ -0,0 +1,79 @@
+package rabbitmq
+
+import (
+	"fmt"
+
+	m "github.com/7574-sistemas-distribuidos/tp-mom/golang/internal/middleware"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type queueMiddleware struct {
+	conn      *amqp.Connection
+	channel   *amqp.Channel
+	queueName string
+}
+
+func NewQueueMiddleware(queueName string, connectionSettings m.ConnSettings) (m.Middleware, error) {
+	conn, err := amqp.Dial(fmt.Sprintf("amqp://guest:guest@%s:%d/", connectionSettings.Hostname, connectionSettings.Port))
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+	defer ch.Close()
+
+	_, err = ch.QueueDeclare(
+		"hello", // name
+		true,    // durability
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		amqp.Table{
+			amqp.QueueTypeArg: amqp.QueueTypeQuorum,
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &queueMiddleware{
+		conn:      conn,
+		channel:   ch,
+		queueName: queueName,
+	}, nil
+}
+
+// Comienza a escuchar a la cola/exchange e invoca a callbackFunc tras
+// cada mensaje de datos o de control con el cuerpo del mensaje.
+// callbackFunc tiene como parámetro:
+// msg - El struct tal y como lo recibe el método Send.
+// ack - Una función que hace ACK del mensaje recibido.
+// nack - Una función que hace NACK del mensaje recibido.
+// Si se pierde la conexión con el middleware devuelve ErrMessageMiddlewareDisconnected.
+// Si ocurre un error interno que no puede resolverse devuelve ErrMessageMiddlewareMessage.
+func (*queueMiddleware) StartConsuming(callbackFunc func(msg m.Message, ack func(), nack func())) (err error) {
+	return nil
+}
+
+// Si se estaba consumiendo desde la cola/exchange, se detiene la escucha. Si
+// no se estaba consumiendo de la cola/exchange, no tiene efecto, ni levanta
+// Si se pierde la conexión con el middleware devuelve ErrMessageMiddlewareDisconnected.
+func (*queueMiddleware) StopConsuming() {}
+
+// Envía un mensaje a la cola o a los tópicos con el que se inicializó el exchange.
+// Si se pierde la conexión con el middleware devuelve ErrMessageMiddlewareDisconnected.
+// Si ocurre un error interno que no puede resolverse devuelve ErrMessageMiddlewareMessage.
+func (*queueMiddleware) Send(msg m.Message) (err error) {
+	return nil
+}
+
+// Se desconecta de la cola o exchange al que estaba conectado.
+// Si ocurre un error interno que no puede resolverse devuelve ErrMessageMiddlewareClose.
+func (*queueMiddleware) Close() error {
+	return nil
+}
